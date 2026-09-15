3eb34de45e8ffe892f66aef87aed2c129a50277e
Subam <subamspkt10@gmail.com>
2026-09-15 16:16:05 -0500

    Update welcome message

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
             <title>Course Server</title>
         </head>
         <body>
-            <h1>Hello from my server!</h1>
+            <h1>Hello from Subam's Server!</h1>
             <p>This page is being served by Node.js and Express.</p>
         </body>
         </html>
